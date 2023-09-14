b15c06f75b31fdb7b051d6f10a1892ccb947e544
Andrés Alvarez <126286883+Gonther1@users.noreply.github.com>
2023-09-14 18:13:37 +0000

    feat: :tada: New Project
    @Gonther1

---
+++ exercise/LinqQueries.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class LinqQueries
+{
+    List<Book> lstBook = new List<Book>();
+
+    public LinqQueries()
+    {
+        using (StreamReader reader = new StreamReader("books.json")){
+            string json= reader.ReadToEnd();
+            this.lstBook=System.Text.Json.JsonSerializer
+            .Deserialize<List<Book>>(json,new System.Text.Json.JsonSerializerOptions()
+            {PropertyNameCaseInsensitive = true }) ?? new List<Book>();
+        }
+    }
+    public IEnumerable<Book> AllCollection()
+    {
+        return lstBook;
+    }
+    public IEnumerable<Book> LibrosDespues2000()
+    {
+        // return lstBook.Where(book => book.PublishedDate.Year > 2000);
+        return from book in lstBook where book.PublishedDate.Year > 200 select book;
+    }
+    public IEnumerable<Book> Libros200Pages()
+    {
+        return from book in lstBook where book.PageCount > 250 && book.Title.Contains("in Action") select book;
+    }
+    public bool CampoStatus()
+    {
+        // return from book in lstBook.All(book => book.Status != String.Empty);
+        return lstBook.All(book => book.Status != String.Empty);
+    }
+    public IEnumerable<Book> Publish2005()
+    {
+        if (lstBook.Any(book => book.PublishedDate.Year == 2007))
+        {
+            return from book in lstBook where book.PublishedDate.Year == 2007 select book;
+        }
+        else 
+        {
+            return default;
+        }
+    }
+}
---
+++ exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool bolean;
+        LinqQueries queries = new LinqQueries();
+        // ImprimirValores(queries.AllCollection());
+        // ImprimirValores(queries.Libros200Pages());
+        // bolean=queries.CampoStatus();
+        // if (bolean==true)
+        // {
+        //     Console.WriteLine("Todos los libros contienen un status");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("Al menos algun libro no tiene status");
+        // }
+
+        ImprimirValores(queries.Publish2005());
+    }
+    private static void ImprimirValores(IEnumerable<Book> books)
+    {
+        int registros=0;
+        Console.Clear();
+        Console.ForegroundColor =  ConsoleColor.Yellow;
+        Console.WriteLine("{0,-70} {1,7} {2,20}", "Titulo", "N. Paginas", "Fecha publicacion");
+        foreach(Book book in books)
+        {
+            Console.ForegroundColor =  ConsoleColor.Blue;
+            registros+=1;
+            Console.WriteLine("{0,-70} {1,7} {2,20}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+        }
+    }
+}
